b3f6fcc922e2db92f2b927d763569cd953553d79
Kamil_Koczan <kamil.koczan@naviparking.com>
2024-04-18 07:50:27 +0200

    Wrote test

--- src/test/kotlin/mergeSortedArrays/MergeSortedArraysTest.kt
+++ src/test/kotlin/mergeSortedArrays/MergeSortedArraysTest.kt
@@ -1,16 +1,19 @@
 package mergeSortedArrays
 
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.collections.shouldBeIn
+import io.kotest.matchers.collections.shouldContainInOrder
 
 class MergeSortedArraysTest : FunSpec({
 
     test("merge") {
-        val arr = mutableListOf(4, 5, 6, 0, 0, 0).toIntArray()
+        val arr = intArrayOf(4, 5, 6, 0, 0, 0)
         MergeSortedArrays().merge(
             arr,
             3,
             mutableListOf(1, 2, 3).toIntArray(),
             3
         )
+        arr.toList().shouldContainInOrder(1, 2, 3, 4, 5, 6)
     }
 })
