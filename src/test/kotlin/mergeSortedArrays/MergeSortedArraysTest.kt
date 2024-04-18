f966e5a9ac130f3627e1e0e55e523e7990ba8ae0
Kamil_Koczan <kamil.koczan@naviparking.com>
2024-04-18 07:58:57 +0200

    Wrote tests

--- src/test/kotlin/mergeSortedArrays/MergeSortedArraysTest.kt
+++ src/test/kotlin/mergeSortedArrays/MergeSortedArraysTest.kt
@@ -1,19 +1,25 @@
 package mergeSortedArrays
 
-import io.kotest.core.spec.style.FunSpec
-import io.kotest.matchers.collections.shouldBeIn
+import io.kotest.core.spec.style.ShouldSpec
 import io.kotest.matchers.collections.shouldContainInOrder
 
-class MergeSortedArraysTest : FunSpec({
+class MergeSortedArraysTest : ShouldSpec({
 
-    test("merge") {
+    lateinit var solution: MergeSortedArrays
+
+    beforeTest {
+        solution = MergeSortedArrays()
+    }
+
+    should("join two sorted arrays case #1") {
         val arr = intArrayOf(4, 5, 6, 0, 0, 0)
-        MergeSortedArrays().merge(
-            arr,
-            3,
-            mutableListOf(1, 2, 3).toIntArray(),
-            3
-        )
+        solution.merge(arr, 3, intArrayOf(1, 2, 3), 3)
         arr.toList().shouldContainInOrder(1, 2, 3, 4, 5, 6)
+    }
+
+    should("join two sorted arrays case #2") {
+        val arr = intArrayOf(1, 2, 3, 0, 0, 0)
+        solution.merge(arr, 3, intArrayOf(2, 5, 6), 3)
+        arr.toList().shouldContainInOrder(1, 2, 2, 3, 5, 6)
     }
 })
